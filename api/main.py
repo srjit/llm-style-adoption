73fdf3643b7dc4ea990924af8b279c45ec4bf2e7
Sreejith S <sreejith2904@gmail.com>
2023-08-28 15:56:08 -0700

    api modifications

--- api/main.py
+++ api/main.py
@@ -7,13 +7,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/get_suggestions/{text:str}")
+@app.get("/get_suggestions")
 async def get_suggestions(text: str):
     generated = suggestions.get(text)
     return {"suggestions": generated}
 
 
-@app.get("/retrain/{notes_path:path}")
+@app.get("/retrain/")
 async def retrain(notes_path: str):
 
     print(f"Retraining with new notes : {notes_path}")
